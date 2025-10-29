7af81bc392f2384f1237e42549f42d98e69c7b3e
Jacob Flory <jakea.flory@gmail.com>
2025-10-29 07:33:51 -0400

    Establishes Base for project
    Completes most of the html and css for the project.

---
+++ scripts.js
@@ -0,0 +1,25 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const taskInput = document.getElementById('taskInput');
+    const addTaskButton = document.getElementById('addTaskButton');
+    const taskList = document.getElementById('taskList');
+
+
+    const addTask = (event) => {
+        event.preventDefault(); // Prevent form submission
+        const taskText = taskInput.value.trim();
+        if (taskText !== '') {
+            return;
+        }
+
+        const li = document.createElement('li');
+        li.textContent = taskText;
+        taskList.appendChild(li);
+        taskInput.value = '';
+    };
+
+    addTaskButton.addEventListener('click', (addTask) => {
+    taskInput.addEventListener('keypress',  (enter) => {
+        if (enter.key === 'Enter') {
+            addTask(enter);
+    })
